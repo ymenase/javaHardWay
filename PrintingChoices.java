afa02141c1cb05a3cf9c39f464769b68fac60a96
Yvette Menase <yvette.j.menase@gmail.com>
2016-09-12 18:24:47 -0600

    adding to repo

---
+++ PrintingChoices.java
@@ -0,0 +1,23 @@
+public class PrintingChoices {
+	public static void main( String[] args ) {
+		System.out.println( "Alpha" );
+		System.out.println( "Bravo" );
+		
+		System.out.println( "Charlie" );
+		System.out.println( "Delta" );
+		System.out.println();
+		
+		System.out.println( "Echo" );
+		System.out.println( "Foxtrot" );
+		
+		System.out.println( "Golf" );
+		System.out.println( "Hotel" );
+		System.out.println();
+		System.out.println( "India" );
+		
+		System.out.println();
+		System.out.println( "I am learning" + " " + "Java" + " " + "the Hard" + " Way!");
+	}
+}			
+		
+				
