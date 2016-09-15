6ee31f50ea7544aa291b9bfdcb5963c36cf54a85
Yvette Menase <yvette.j.menase@gmail.com>
2016-09-15 16:54:07 -0600

    study drill 2

--- BMICalculator.java
+++ BMICalculator.java
@@ -3,13 +3,18 @@ import java.util.Scanner;
 public class BMICalculator {
 	public static void main( String[] args ) {
 		Scanner keyboard = new Scanner(System.in);
-		double pounds, inches, bmi;
+		double feet, inches, pounds, bmi;
 		
-		System.out.print( "Your height in inches: " );
+		System.out.print( "Your height (feet only): " );
+		feet = keyboard.nextDouble();
+		
+		System.out.print( "Your height (inches): " );
 		inches = keyboard.nextDouble();
 		
 		System.out.print( "Your weight in pounds: " );
 		pounds = keyboard.nextDouble();
+		
+		inches = (feet*12) + inches;
 		
 		bmi = (pounds *(.45)) / ((inches*.0254)*(inches*.0254));
 		
