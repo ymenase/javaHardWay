f3c73a8ae86ad3a431e4cf8e1b1aca78e5e34f2c
Yvette Menase <yvette.j.menase@gmail.com>
2016-09-15 17:23:43 -0600

    working program

--- Sequencing.java
+++ Sequencing.java
@@ -2,20 +2,18 @@ import java.util.Scanner;
 
 public class Sequencing {
 	public static void main( String[] args ) {
-		// THIS CODE IS BROKEN UNTIL YOU FIX IT
 		
 		Scanner keyboard = new Scanner(System.in);
 		double price = 0, salesTax, total;
 		
-		salesTax = price * 0.0825;
-		total = price + salesTax;
-		
 		System.out.print( "How much is the purchase price? " );
 		price = keyboard.nextDouble();
+		
+		salesTax = price * 0.0825;
+		total = price + salesTax;
 		
 		System.out.println( "Item price:\t" + price );
 		System.out.println( "Sales tax:\t" + salesTax );
 		System.out.println( "Total cost:\t" + total );
 	}
 }
-//won't compile if 0 is removed because price isn't defined
