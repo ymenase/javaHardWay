ff7b915117fb3875e3310a3b4194b529d2f1492b
Yvette Menase <yvette.j.menase@gmail.com>
2016-09-15 22:35:21 -0600

    Study drill 1

--- WeaselOrNot.java
+++ WeaselOrNot.java
@@ -9,10 +9,11 @@ public class WeaselOrNot {
 		System.out.println ("Type the word \"weasel\", please." );
 		word = keyboard.next();
 		
-		yep = word.equals("weasel");
+		yep = "weasel".equals(word);
 		nope = ! word.equals("weasel");
 		
 		System.out.println( "You typed what was requested: " + yep );
 		System.out.println( "You ignored polite instructions. " + nope );
 	}
 }
+// it works
