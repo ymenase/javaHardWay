44f2fd64e229d220ddb820d4d1b67e75741fdefb
Yvette Menase <yvette.j.menase@gmail.com>
2016-09-17 13:52:46 -0600

    Study Drill 2, adding a question

--- AgeMessages.java
+++ AgeMessages.java
@@ -11,20 +11,19 @@ public class AgeMessages {
 		System.out.println( "You are: " );
 		if (age < 13 ) {
 			System.out.println( "\ttoo young to create a Facebook account" );
-		}
-		
+		}		
 		if (age < 15 ) {
 			System.out.println( "\ttoo young to get a driver's license" );
-		}
-		
+		}	
 		if ( age < 18 ) {
 			System.out.println( "\ttoo young to get a tattoo" );
-		}
+		}	
 		if ( age < 35 ) {
 			System.out.println( "\ttoo young to run for President of the U.S." );
+		}	
+		if ( age >= 65 ) {	
+			System.out.println( "\tYou are old enough to retire!" );
 			System.out.println( "\t\t(How sad!)" );
 		}
 	}
 }
-// if I enter an age over 35, nothing prints because ints greater than 35 
-// are outside the criteria
