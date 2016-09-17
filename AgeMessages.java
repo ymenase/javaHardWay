796262165ce2bceea1e0ddcf8b060731de5bd513
Yvette Menase <yvette.j.menase@gmail.com>
2016-09-17 13:57:53 -0600

    Study drill 3: adding opposites

--- AgeMessages.java
+++ AgeMessages.java
@@ -10,19 +10,34 @@ public class AgeMessages {
 		
 		System.out.println( "You are: " );
 		if (age < 13 ) {
-			System.out.println( "\ttoo young to create a Facebook account" );
+			System.out.println( "\tToo young to create a Facebook account" );
 		}		
+		if (age >= 13 ) {
+			System.out.println( "\tOld enough to create a Facebook account" );
+		}
 		if (age < 15 ) {
-			System.out.println( "\ttoo young to get a driver's license" );
+			System.out.println( "\tToo young to get a driver's license" );
 		}	
+		if (age >= 15 ) {
+			System.out.println( "\tOld enough to get a driver's license" );
+		}
 		if ( age < 18 ) {
 			System.out.println( "\ttoo young to get a tattoo" );
+		}	
+		if ( age >= 18 ) {
+			System.out.println( "\tOld enough to get a tattoo" );
 		}	
 		if ( age < 35 ) {
 			System.out.println( "\ttoo young to run for President of the U.S." );
 		}	
+		if ( age >=35 ) {
+			System.out.println( "\tOld enough to run for President of the U.S." );
+		}
 		if ( age >= 65 ) {	
 			System.out.println( "\tYou are old enough to retire!" );
+		}
+		if (age <  65 ) {
+			System.out.println( "\tYou are too young to retire!" );
 			System.out.println( "\t\t(How sad!)" );
 		}
 	}
