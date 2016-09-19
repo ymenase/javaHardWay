f30b8243052eede9f5c481626954119564ee7eac
Yvette Menase <yvette.j.menase@gmail.com>
2016-09-19 14:44:18 -0600

    Study drill 1: changing loop type

--- RunningTotal.java
+++ RunningTotal.java
@@ -1,19 +1,24 @@
 import java.util.Scanner;
 
-public class RunningTotal {
-	public static void main( String[] args ) {
+public class RunningTotal 
+{
+	public static void main( String[] args ) 
+	{
 		Scanner keyboard = new Scanner(System.in);
-		int current, total = 0;
+	
+		int current = 1, total = 0;  
 		
 		System.out.print( "Type in a bunch of values and I'll add them up. ");
 		System.out.println( "I'll stop when you type a zero." );
 		
-		do {
+		 while ( current != 0 )  
+		 {
 			System.out.print("Value: ");
 			current = keyboard.nextInt();
-			total += current;
+			int newtotal = current + total;
+			total = newtotal;
 			System.out.println( "The total so far is: "  + total);
-		} while ( current != 0 );
+		}
 		
 		System.out.println( "The final total is: " + total);
 	}
