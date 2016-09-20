6360e2ec27324a84abab5b3e796b7f8dea370791
Yvette Menase <yvette.j.menase@gmail.com>
2016-09-20 16:55:31 -0600

    practice not in book

---
+++ practicetyping.java
@@ -0,0 +1,246 @@
+//if statements
+if (text){
+	//code
+}
+
+if (text) {
+	//code
+}
+
+if (text) {
+	//code
+}
+
+if (text) {
+	//code
+}
+
+if (text) {
+	//code
+}
+
+if (text) {
+	//code
+}
+
+if (text) {
+	//code
+}
+
+if (text) {
+	//code
+}
+
+if (text) {
+	//code
+}
+
+if (text) {
+	//code
+}
+
+if (text) {
+	//code
+} else {
+	//code
+}
+
+if (text) {
+	//code
+} else {
+	//code
+}
+
+if (text) {
+	//code
+} else {
+	//code
+}
+	
+if (text) {
+	//code
+} else {
+	//code
+}
+	
+if (text) {
+	//code
+} else {
+	//code
+}
+
+if (text) {
+	//code
+} else {
+	//code
+}
+
+if (text) {
+	//code
+} else {
+	//code
+}
+
+if (text) {
+	//code
+} else {
+	//code
+}
+
+
+//while loops
+
+while (text) {
+	//code
+}
+
+while (text) {
+	//code
+}
+
+while (text) {
+	//code
+}
+
+while (text) {
+	//code
+}
+
+while (text) {
+	//code
+}
+
+while (text) {
+	//code
+}
+
+while (text) {
+	//code
+}
+
+
+//do-while loops
+
+do { 
+	//code
+} while (test);
+
+do {
+	//code
+} while (test);
+
+do { 
+	//code
+} while (test);
+
+do {
+	//code
+} while (test);
+
+do { 
+	//code
+} while (test);
+
+do { 
+	//code
+} while (test);
+
+do { 
+	//code
+} while (test);
+
+//Calling a Function
+functionName(args);
+functionName(args);
+functionName(args);
+functionName(args);
+functionName(args);
+functionName(args);
+functionName(args);
+functionName(args);
+functionName(args);
+
+//Writing a Function
+public static void functionName(parameters) {
+	//code
+}
+
+public static void functionName(parameters) {
+	//code
+}
+
+public static void functionName(parameters) {
+	//code
+}
+
+public static void functionName(parameters) {
+	//code
+}
+
+public static void functionName(parameters) {
+	//code
+}
+
+public static void functionName(parameters) {
+	//code
+}
+
+public static void functionName(parameters) {
+	//code
+}
+
+// Main Method
+public static void main( String[] args ) {
+	//code
+}
+
+public static void main( String[] args ) {
+	//code
+}
+
+public static void main( String[] args ) {
+	//code
+}
+
+public static void main( String[] args ) {
+	//code
+}
+
+public static void main( String[] args ) {
+	//code
+}
+
+public static void main( String[] args ) {
+	//code
+}
+
+//import java.util.Scanner
+
+import java.util.Scanner;
+import java.util.Scanner;
+import java.util.Scanner;
+import java.util.Scanner;
+import java.util.Scanner;
+import java.util.Scanner;
+import java.util.Scanner;
+import java.util.Scanner;
+import java.util.Scanner;
+import java.util.Scanner;
+
+// create scanner object
+
+Scanner keyboard = new Scanner(System.in);
+Scanner keyboard = new Scanner(System.in);
+Scanner keyboard = new Scanner(System.in);
+Scanner keyboard = new Scanner(System.in);
+Scanner keyboard = new Scanner(System.in);
+Scanner keyboard = new Scanner(System.in);
+Scanner keyboard = new Scanner(System.in);
+
+//creating a range
+10*math.random()
+10*math.random()
+10*math.random()
+10*math.random()
+11*math.random()
+12*math.random()
