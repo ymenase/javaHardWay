af6328979ce30de4589106981ca512f08298090c
Yvette Menase <yvette.j.menase@gmail.com>
2016-09-21 23:02:28 -0600

    Study drill 2

--- HeronsFormula.java
+++ HeronsFormula.java
@@ -14,13 +14,14 @@ public class HeronsFormula {
 		System.out.println(tws + "5,12,13 has area " + triangleArea(5, 12, 13) );
 		System.out.println(tws + "10,9,11 has area " + triangleArea(10, 9, 11) );
 		System.out.println(tws + "8,15,17 has area " + triangleArea(8, 15, 17) );
+		System.out.println(tws + "9,9,9 has area " + triangleArea(9, 9, 9) );
 	}
 
 	//This function computes the area of a triangle with side lengths a, b, & c.
 	public static double triangleArea( int a, int b, int c ) {
 		double s, A;
 
-	s = (a+b+c) / 2;
+	s = (a+b+c) / 2.0;
 	A = Math.sqrt( s*(s-a)*(s-b)*(s-c) );
 
 		// After computing the area, you must "return" the computed value:
@@ -28,3 +29,5 @@ public class HeronsFormula {
 	}
 }
 //drill 1: the version that does not use a function is longer
+//drill 2: it would have been harder to change without the function 
+//because there would be more places to change it and higher room for error
